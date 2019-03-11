aaac25cf25d13b4ed14f4390643789e256d970e3
HankRobot <thc1n17@soton.ac.uk>
2019-03-11 21:44:06 +0800

    ITS DONE!

--- InsanityCT/InsanityCT.ino
+++ InsanityCT/InsanityCT.ino
@@ -15,31 +15,31 @@ Cytron_G15Shield g15(2, 3, 8); // SoftwareSerial: Rx, Tx and Control pin
 void setup() {
   //initialize the arduino main board's serial/UART and Control Pins
   //CTRL pin for G15 =3 and AX12 =8
+  for(int i = 4; i < 8; i++)
+  {
+    pinMode(i, INPUT);    // sets the digital pin 7 as INPUT
+    digitalWrite(i, HIGH); 
+  }
+
   g15.begin(115200); 
   g15.setWheelMode(G15_lf);
   g15.setWheelMode(G15_lb);
   g15.setWheelMode(G15_rf);
   g15.setWheelMode(G15_rb);
+  
+  //first step
   g15.exitWheelMode(G15_dl);
   g15.exitWheelMode(G15_dr);
   g15.exitWheelMode(G15_s);
+  setangle(G15_dl,270);
+  setangle(G15_dr,90);
+  setangle(G15_s,350);
+  delay(1000);
   Serial.begin(9600); //Start the serial on computer
   delay(1000);
 }
 
-void turnleft(int speed, int time){
-  g15.setWheelMode(G15_lf);
-  g15.setWheelMode(G15_lb);
-  g15.setWheelMode(G15_rf);
-  g15.setWheelMode(G15_rb);
-  g15.setWheelSpeed(G15_lf,speed,CCW); 
-  g15.setWheelSpeed(G15_lb,speed,CCW);
-  g15.setWheelSpeed(G15_rf,speed,CCW);
-  g15.setWheelSpeed(G15_rb,speed,CCW);
-  delay(time);
-}
-
-void turnright(int speed, int time){
+void turnleft(int speed, int timee){
   g15.setWheelMode(G15_lf);
   g15.setWheelMode(G15_lb);
   g15.setWheelMode(G15_rf);
@@ -48,10 +48,22 @@ void turnright(int speed, int time){
   g15.setWheelSpeed(G15_lb,speed,CW);
   g15.setWheelSpeed(G15_rf,speed,CW);
   g15.setWheelSpeed(G15_rb,speed,CW);
-  delay(time);
+  delay(timee);
 }
 
-void moveforward(int speed, int time){
+void turnright(int speed, int timee){
+  g15.setWheelMode(G15_lf);
+  g15.setWheelMode(G15_lb);
+  g15.setWheelMode(G15_rf);
+  g15.setWheelMode(G15_rb);
+  g15.setWheelSpeed(G15_lf,speed,CCW); 
+  g15.setWheelSpeed(G15_lb,speed,CCW);
+  g15.setWheelSpeed(G15_rf,speed,CCW);
+  g15.setWheelSpeed(G15_rb,speed,CCW);
+  delay(timee);
+}
+
+void moveforward(int speed, int timee){
   g15.setWheelMode(G15_lf);
   g15.setWheelMode(G15_lb);
   g15.setWheelMode(G15_rf);
@@ -60,10 +72,10 @@ void moveforward(int speed, int time){
   g15.setWheelSpeed(G15_lb,speed,CCW);
   g15.setWheelSpeed(G15_rf,speed,CW);
   g15.setWheelSpeed(G15_rb,speed,CW);
-  delay(time);
+  delay(timee);
 }
 
-void movebackward(int speed, int time){
+void movebackward(int speed, int timee){
   g15.setWheelMode(G15_lf);
   g15.setWheelMode(G15_lb);
   g15.setWheelMode(G15_rf);
@@ -72,30 +84,32 @@ void movebackward(int speed, int time){
   g15.setWheelSpeed(G15_lb,speed,CW);
   g15.setWheelSpeed(G15_rf,speed,CCW);
   g15.setWheelSpeed(G15_rb,speed,CCW);
-  delay(time);
+  delay(timee);
 }
 
-void stop_motion(int time){
+void stop_motion(int timee){
   g15.exitWheelMode(G15_lf);
   g15.exitWheelMode(G15_lb);
   g15.exitWheelMode(G15_rf);
   g15.exitWheelMode(G15_rb);
-  delay(time);
+  delay(timee);
 }
 
 void turnangle(int start,int endd,double timee, int ID){
   double x = abs((((start-endd)/timee)/378)  * 1023);
-  Serial.println(x);
   g15.setSpeed(ID,x);
   g15.setPosAngle(ID,endd);
 }
 
 void setangle(int ID,int start){
-  g15.setSpeed(ID, 200); // Set G15 (ID = 1) speed to 500,
+  g15.setSpeed(ID, 100); // Set G15 (ID = 1) speed to 500,
   g15.setPosAngle(ID, start);
 }
 
 void excavate(){
+  g15.exitWheelMode(G15_dl);
+  g15.exitWheelMode(G15_dr);
+  g15.exitWheelMode(G15_s);
   //first step
   setangle(G15_dl,270);
   setangle(G15_dr,90);
@@ -129,19 +143,32 @@ void excavate(){
 }
 
 void shake(){
-
+  turnleft(500,100);
+  turnright(500,100);
 }
 
-void pullup(){
-  
+void pullup(int speed, int timee){
+  g15.setWheelMode(G15_s);
+  g15.setWheelSpeed(G15_s,speed,CW); 
+  delay(timee);
+  g15.exitWheelMode(G15_s);
 }
 
-void pulldown(){
-  
+void pulldown(int speed, int timee){
+  g15.setWheelMode(G15_s);
+  g15.setWheelSpeed(G15_s,speed,CCW); 
+  delay(timee);
+  g15.exitWheelMode(G15_s);
 }
 
 void loop() {
   /*
+  Serial.print(digitalRead(7));
+  Serial.print(digitalRead(6));
+  Serial.print(digitalRead(5));
+  Serial.print(digitalRead(4));
+  Serial.println("");
+  */
   if (digitalRead(7)==LOW && digitalRead(6)==LOW && digitalRead(5)==LOW && digitalRead(4)==HIGH) { //0001
     turnleft(500,0);
   }
@@ -160,16 +187,14 @@ void loop() {
   else if (digitalRead(7)==LOW && digitalRead(6)==HIGH && digitalRead(5)==HIGH && digitalRead(4)==LOW) { //0110
     shake();
   }
-  else if (digitalRead(7)==LOW && digitalRead(6)==HIGH && digitalRead(5)==HIGH && digitalRead(4)==HIGH) { //0111
-    pullup();
+  else if (digitalRead(7)==HIGH && digitalRead(6)==LOW && digitalRead(5)==LOW && digitalRead(4)==HIGH) { //0111
+    pullup(500,10);
   }
-  else if (digitalRead(7)==HIGH && digitalRead(6)==LOW && digitalRead(5)==LOW && digitalRead(4)==LOW) { //0111
-    pulldown();
+  else if (digitalRead(7)==HIGH && digitalRead(6)==LOW && digitalRead(5)==HIGH && digitalRead(4)==LOW) { //0111
+    pulldown(500,10);
   }
   else {
     stop_motion(10);
   }
-  */
-  excavate();
-  delay(5000);
+  //excavate();
 }
